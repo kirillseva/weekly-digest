76adcb4b967234b127daf80536ec7f3ab673ccf0
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-08-17 18:46:38 -0400

    removed hello to remove flickering

--- client/app/description/description.controller.js
+++ client/app/description/description.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('weeklyDigestApp')
   .controller('DescriptionCtrl', function ($scope, $http) {
-    $scope.about = 'Hello';
+    $scope.about = '';
 
     $http.get('/api/descriptions').success(function(about) {
       $scope.about = about.desc;

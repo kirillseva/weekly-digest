2e3e0079391c78b650172303f00c8767d5b07ade
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-08-19 22:37:00 -0400

    ported 8tracks related code to a service

--- client/app/EightTracks/EightTracks.service.js
+++ client/app/EightTracks/EightTracks.service.js
@@ -1,6 +1,23 @@
 'use strict';
+var api = '8a19d841543a1017318d5afbc73b2fb120101e54';
+var username = 'kirillseva';
+var smart_id = 'dj:9338738';
 
 angular.module('weeklyDigestApp')
-  .service('EightTracks', function () {
+  .service('EightTracks', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    var EightTracks = {};
+    $http.get('http://8tracks.com/users/'+ username +'/mixes.json?api_key=' + api + '?api_version=3').success(function(all_mixes) {
+      var latest_mix = all_mixes.mix_set.mixes[1];
+      var mix_id = latest_mix.id;
+      EightTracks.name = latest_mix.name;
+      EightTracks.picture = latest_mix.cover_urls.sq500;
+      // Now, time to get the mix itself
+      $http.get('http://8tracks.com/mixes/'+ mix_id +'.json?api_key=' + api + '?api_version=3').success(function(mix) {
+        EightTracks.resp = mix;
+      });
+    });
+
+
+    return EightTracks;
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,23 +1,8 @@
 'use strict';
-var api = '8a19d841543a1017318d5afbc73b2fb120101e54';
-var username = 'kirillseva';
-var smart_id = 'dj:9338738';
+
 
 angular.module('weeklyDigestApp')
-.controller('MainCtrl', function ($scope, $http) {
-  $scope.resp = "";
-  $scope.picture = "";
+.controller('MainCtrl', function ($scope, $http, EightTracks) {
+  $scope.EightTracks = EightTracks;
 
-  $http.get('http://8tracks.com/users/'+ username +'/mixes.json?api_key=' + api + '?api_version=3').success(function(all_mixes) {
-    var latest_mix = all_mixes.mix_set.mixes[1];
-    var mix_id = latest_mix.id;
-    $scope.name = latest_mix.name;
-    $scope.picture = latest_mix.cover_urls.sq500;
-
-    // Now, time to get the mix itself
-    $http.get('http://8tracks.com/mixes/'+ mix_id +'.json?api_key=' + api + '?api_version=3').success(function(mix) {
-      $scope.resp = mix;
-    });
-
-  });
 });

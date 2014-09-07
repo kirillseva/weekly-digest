629657e9d0169c7de085f29205ec91e62b7d067d
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-09-07 16:14:37 -0400

    turning newrelic on

--- server/app.js
+++ server/app.js
@@ -6,8 +6,7 @@
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-require('newrelic');
-
+var newrelic = require('newrelic');
 var express = require('express');
 var config = require('./config/environment');
 // Setup server

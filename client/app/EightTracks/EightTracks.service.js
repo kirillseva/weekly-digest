a718274e2948d8ac4e75f393380a54687204f1a0
Kirill Sevastyanenko <kirillseva@gmail.com>
2014-09-06 23:14:10 -0400

    major refactoring. Was too much spaghetti

--- client/app/EightTracks/EightTracks.service.js
+++ client/app/EightTracks/EightTracks.service.js
@@ -1,33 +1,30 @@
 'use strict';
-var api = '8a19d841543a1017318d5afbc73b2fb120101e54';
+var api = 'api_key=8a19d841543a1017318d5afbc73b2fb120101e54?api_version=3';
 var username = 'kirillseva';
-var smart_id = 'dj:9338738';
+var djId = 'dj:9338738';
 
 angular.module('weeklyDigestApp')
-  .service('EightTracks', function ($http, $document, Audio) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-    var EightTracks = {};
-    $http.get('http://8tracks.com/users/'+ username +'/mixes.json?api_key=' + api + '?api_version=3').success(function(all_mixes) {
-      var latest_mix = all_mixes.mix_set.mixes[1];
-      var mix_id = latest_mix.id;
-      EightTracks.name = latest_mix.name;
-      EightTracks.picture = latest_mix.cover_urls.sq250;
-      // in order to play songs, we need to create a token
-      $http.get('http://8tracks.com/sets/new.json?api_key=' + api + '?api_version=3').success(function(new_scope) {
-        // Now, time to get the mix itself
-        var token = new_scope.play_token;
-        $http.get('http://8tracks.com/sets/'+ token +'/play.json?mix_id='+mix_id+'?api_key=' + api + '?api_version=3').success(function(mix) {
-          // EightTracks.resp = mix;
-          var url = mix.set.track.track_file_stream_url;
-          // EightTracks.resp = url;
-          EightTracks.audio = Audio;
-          EightTracks.audio.src = url;          
-        });
-      });
-
-
-    });
-
-
-    return EightTracks;
-  });
+.service('EightTracks', function ($rootScope, $http, $document) {
+  // AngularJS will instantiate a singleton by calling "new" on this function
+  var EightTracks = {
+    getPlayToken: function() {
+      return $http.get("http://8tracks.com/sets/new.json?" + api)
+    },
+    getMixes: function(page) {
+      return $http.get("http://8tracks.com/mix_sets/" + djId + ".json?include=mixes&page=" + page + "&per_page=100&" + api);
+    },
+    getSet: function(mix) {
+      return $http.get("http://8tracks.com/sets/" + $rootScope.playToken + "/play.json?mix_id=" + mix + "&" + api);
+    },
+    skip: function(mix) {
+      return $http.get("http://8tracks.com/sets/" + $rootScope.playToken + "/skip.json?mix_id=" + mix + "&" + api);
+    },
+    next: function(mix) {
+      return $http.get("http://8tracks.com/sets/" + $rootScope.playToken + "/next.json?mix_id=" + mix + "&" + api);
+    },
+    reportSong: function(track_id, mix_id) {
+      return $http.get("http://8tracks.com/sets/" + $rootScope.playToken + "/report.xml?track_id=" + track_id + "&mix_id=" + mix_id);
+    }
+  };
+  return EightTracks;
+});
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,7 +2,16 @@
 
 
 angular.module('weeklyDigestApp')
-.controller('MainCtrl', function ($scope, $http, EightTracks) {
-  $scope.EightTracks = EightTracks;
+.controller('MainCtrl', function ($scope, $http, EightTracks, Audio) {
+  // first, create a play token for the user
+  EightTracks.getPlayToken()
+  .success(function(data) {
+    $scope.playToken = data.play_token;
+  })
+  .error(function() {
+    console.log('Unable to create play token.');
+  });
 
+
+  $scope.Audio = Audio;
 });
